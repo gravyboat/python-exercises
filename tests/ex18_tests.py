764e986d9ce35ceaefbf60e98b2eabce7b3837f9
Forrest Alvarez <forrest.alvarez@gmail.com>
2016-05-06 16:26:30 -0700

    Fix a function name

--- tests/ex18_tests.py
+++ tests/ex18_tests.py
@@ -7,11 +7,11 @@ def test_check_if_pangram():
     Check if string is a pangram
     '''
 
-    is_pangram = ex18.check_if_pangram('zxvbcnmasw qedfrtghyujkiolp')
+    is_pangram = ex18.check_if_pangram('abcdefghijklmnopqrstuvwxyz')
     assert_true(is_pangram)
 
 
-def test_check_if_pangram():
+def test_check_if_not_pangram():
     '''
     Check if string is not a pangram
     '''
